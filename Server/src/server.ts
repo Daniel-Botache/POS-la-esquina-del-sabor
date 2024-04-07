765411d5cd2b205cb819efe747700543b848c567
Daniel-Botache <daniel.botache@gmail.com>
2024-04-07 17:49:56 -0500

    se configura la entrada al servidor

---
+++ Server/src/server.ts
@@ -0,0 +1,15 @@
+import { sequelize } from "./infrastructure/config/database";
+import { app } from "./infrastructure/config/app";
+
+// access to PORT from .env file
+const PORT_ENV: string | undefined = process.env.PORT;
+
+//Assign 3000 as default value in case PORT_ENV is undefined
+const PORT = parseInt(PORT_ENV || "3000", 10);
+
+sequelize.sync({ alter: true }).then(() => {
+  app.listen(PORT, () => {
+    // PORT variable used to access the server
+    console.log(`server running on port ${PORT}`);
+  });
+});
