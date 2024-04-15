bb1d79a492a873e830afdbdffe1396cc409edadd
Daniel-Botache <daniel.botache@gmail.com>
2024-04-15 03:44:12 -0500

    se hace la instancia para el usuario

--- Server/src/application/repositories/IClientRepository.ts
+++ Server/src/application/repositories/IClientRepository.ts
@@ -1,9 +1,9 @@
-import { ClientInstance } from "../../domain/models/ClientAtrributes";
+import { UserInstance } from "../../domain/models/UserAttributes";
 
-export interface IClientRepository {
-  findAll(): Promise<ClientInstance[]>;
-  findById(id: number): Promise<ClientInstance | null>;
-  create(id: ClientInstance): Promise<ClientInstance>;
-  update(id: number, client: ClientInstance): Promise<void>;
+export interface IUserRepository {
+  findAll(): Promise<UserInstance[]>;
+  findById(nick: string): Promise<UserInstance | null>;
+  create(id: UserInstance): Promise<UserInstance>;
+  update(id: number, client: UserInstance): Promise<void>;
   delete(id: number): Promise<void>;
 }
