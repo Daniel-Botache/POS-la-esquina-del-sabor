27dcee95a90f753dfe3c7dff34acd44d5d14cac1
Daniel-Botache <daniel.botache@gmail.com>
2024-04-20 11:45:33 -0500

    se agregan casos de uso al repositorio de productos

--- Server/src/application/repositories/IProductRepository.ts
+++ Server/src/application/repositories/IProductRepository.ts
@@ -1,7 +1,9 @@
 import { ProductInstance } from "../../domain/models/ProductAttributes";
 
 export interface IProductRepository {
+  findAll(): Promise<ProductInstance[]>;
   findById(id: string): Promise<ProductInstance | null>;
   create(data: ProductInstance): Promise<boolean>;
   delete(id: number): Promise<boolean>;
+  update(id: string, data: ProductInstance): Promise<boolean>;
 }
