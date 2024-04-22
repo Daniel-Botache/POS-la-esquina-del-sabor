13979affdd796644f6564bfe5c65efc775737fb9
Daniel-Botache <daniel.botache@gmail.com>
2024-04-22 01:09:53 -0500

    se agrega el controlador para encontrar todos los productos

--- Server/src/infrastructure/webserver/ProductController.ts
+++ Server/src/infrastructure/webserver/ProductController.ts
@@ -3,4 +3,26 @@ import { Request, Response } from "express";
 import { DefaultController } from "./DefaultController";
 import { sequelize } from "../config/database";
 
-export class ProductController extends DefaultController {}
+export class ProductController extends DefaultController {
+  private productRepository: ProductRepository;
+  constructor() {
+    super(sequelize.models.Product);
+    this.productRepository = new ProductRepository();
+  }
+  findAllProducts = async (req: Request, res: Response) => {
+    try {
+      const productsData = await this.productRepository.findAll();
+      if (productsData.length > 0) {
+        return res
+          .status(200)
+          .json({ success: productsData, message: "Datos encontrados" });
+      }
+      return res
+        .status(404)
+        .json({ success: productsData, message: "Datos no encontrados" });
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).send(err.message);
+    }
+  };
+}
