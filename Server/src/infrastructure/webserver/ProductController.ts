8b88ad5de05c1fed46c57766d7374dd493b8a9bb
Daniel-Botache <daniel.botache@gmail.com>
2024-04-22 01:26:31 -0500

    se agrega el findById al controller de productos

--- Server/src/infrastructure/webserver/ProductController.ts
+++ Server/src/infrastructure/webserver/ProductController.ts
@@ -9,7 +9,7 @@ export class ProductController extends DefaultController {
     super(sequelize.models.Product);
     this.productRepository = new ProductRepository();
   }
-  findAllProducts = async (req: Request, res: Response) => {
+  findAllProducts = async (_req: Request, res: Response) => {
     try {
       const productsData = await this.productRepository.findAll();
       if (productsData.length > 0) {
@@ -20,6 +20,23 @@ export class ProductController extends DefaultController {
       return res
         .status(404)
         .json({ success: productsData, message: "Datos no encontrados" });
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).send(err.message);
+    }
+  };
+  findProductById = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const productData = await this.productRepository.findById(id);
+      if (productData) {
+        return res
+          .status(200)
+          .json({ succes: productData, message: "Datos encontrados" });
+      }
+      return res
+        .status(404)
+        .json({ success: productData, message: "Datos no encontrados" });
     } catch (error) {
       const err = error as Error;
       return res.status(500).send(err.message);
