0c630a891cc0d1f62f3eee1f5fe831bed3170646
Daniel-Botache <daniel.botache@gmail.com>
2024-04-26 12:35:16 -0500

    se hace la estructura para el formulario de usuario

---
+++ Front/src/modules/auth/components/signUpForm.tsx
@@ -0,0 +1,14 @@
+export default function signUpForm(){
+    return(
+        <div>
+            <form action="">
+                <label htmlFor="">Usuario</label>
+                <input type="text" />
+                <input type="password" name="" id="" />
+                <button>Entrar</button>
+            </form>
+            
+        </div>
+
+    )
+}
