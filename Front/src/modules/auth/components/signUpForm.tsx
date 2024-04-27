8405f28aec733e4a4edfce403b40c10f75964bb8
Daniel-Botache <daniel.botache@gmail.com>
2024-04-27 03:08:08 -0500

    se hace el cambio para usar rutas relativas

--- Front/src/modules/auth/components/signUpForm.tsx
+++ Front/src/modules/auth/components/signUpForm.tsx
@@ -1,14 +1,14 @@
-export default function signUpForm(){
-    return(
-        <div>
-            <form action="">
-                <label htmlFor="">Usuario</label>
-                <input type="text" />
-                <input type="password" name="" id="" />
-                <button>Entrar</button>
-            </form>
-            
-        </div>
-
-    )
+function signUpForm() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Usuario</label>
+        <input type="text" />
+        <input type="password" name="" id="" />
+        <button>Entrar</button>
+      </form>
+    </div>
+  );
 }
+
+export { signUpForm };
