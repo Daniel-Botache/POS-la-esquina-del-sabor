1e8eac0378c5d5acda9cff1c4cc7e0b099b8b143
Daniel-Botache <daniel.botache@gmail.com>
2024-04-30 23:17:41 -0500

    se agregan las clases y los iconos a la pantalla de autenticacion

--- Front/src/modules/auth/components/signUpForm.tsx
+++ Front/src/modules/auth/components/signUpForm.tsx
@@ -6,6 +6,8 @@ import { setUserInfo } from "../redux/authSlice";
 import validate from "../validations/validates";
 import { errorMessage } from "../hooks/notifications";
 import { succesMessage } from "../hooks/notifications";
+import iconMarket from "../../../utils/Icons/supermercado.svg.svg";
+import styles from "../styles/SignUpForm.module.css";
 
 export default function SignUpForm() {
   const [username, setUsername] = useState("");
@@ -55,23 +57,38 @@ export default function SignUpForm() {
   }, [access, navigate]);
 
   return (
-    <div>
-      <form onSubmit={loginHandleSubmit}>
-        <input
-          type="text"
-          placeholder="Usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+    <div className={styles.principalContainer}>
+      <div className={styles.formContainer}>
+        <div className={styles.headContainer}>
+          <img
+            src={iconMarket}
+            alt="iconMarket"
+            className={styles.headContainer__img}
+          />
+          <h1 className={styles.headContainer__h1}>POS</h1>
+          <h2 className={styles.headContainer__h2}>La esquina del sabor</h2>
+        </div>
+        <form onSubmit={loginHandleSubmit} className={styles.form}>
+          <input
+            type="text"
+            placeholder="Usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className={styles.form__input}
+          />
 
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Entrar</button>
-      </form>
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={styles.form__input}
+          />
+          <button type="submit" className={styles.form__btn}>
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
