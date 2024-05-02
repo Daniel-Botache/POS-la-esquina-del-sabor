69b74b57fd63d6c7e7332a5bb5b2b9599b655a93
Daniel-Botache <daniel.botache@gmail.com>
2024-05-02 16:45:09 -0500

    se agregan los links del home

--- Front/src/modules/home/components/home.tsx
+++ Front/src/modules/home/components/home.tsx
@@ -8,17 +8,34 @@ import balanceImg from "../../../utils/images/balance.jpg";
 import basesImg from "../../../utils/images/base.jpg";
 import optionsImg from "../../../utils/images/opciones.jpg";
 import styles from "../styles/Home.module.css";
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className={styles.principalContainer}>
-      <MenuCard title={"Ventas"} img={salesImg}></MenuCard>
-      <MenuCard title={"Inventario"} img={stockImg}></MenuCard>
-      <MenuCard title={"Créditos"} img={creditImg}></MenuCard>
-      <MenuCard title={"Ganancias"} img={profitImg}></MenuCard>
-      <MenuCard title={"Gastos"} img={expenseImg}></MenuCard>
-      <MenuCard title={"Balance"} img={balanceImg}></MenuCard>
-      <MenuCard title={"Bases"} img={basesImg}></MenuCard>
-      <MenuCard title={"Opciones"} img={optionsImg}></MenuCard>
+      <Link to={"/sales"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Ventas"} img={salesImg}></MenuCard>
+      </Link>
+      <Link to={"/stock"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Inventario"} img={stockImg}></MenuCard>
+      </Link>
+      <Link to={"/credits"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Créditos"} img={creditImg}></MenuCard>
+      </Link>
+      <Link to={"/profit"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Ganancias"} img={profitImg}></MenuCard>
+      </Link>
+      <Link to={"/expenses"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Gastos"} img={expenseImg}></MenuCard>
+      </Link>
+      <Link to={"/balance"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Balance"} img={balanceImg}></MenuCard>
+      </Link>
+      <Link to={"/bases"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Bases"} img={basesImg}></MenuCard>
+      </Link>
+      <Link to={"/options"} className={styles.principalContainer__Link}>
+        <MenuCard title={"Opciones"} img={optionsImg}></MenuCard>
+      </Link>
     </div>
   );
 }
