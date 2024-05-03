94f8408bd1f679b889265b0b9c18891a358ec007
Daniel-Botache <daniel.botache@gmail.com>
2024-05-03 08:00:46 -0500

    se corrigen estilos del head y se renderiza nombre de usuario

--- Front/src/modules/head/components/head.tsx
+++ Front/src/modules/head/components/head.tsx
@@ -1,17 +1,21 @@
 import { LogoPomo, MarketIcon } from "../../../utils/Icons/icons";
 import style from "../styles/Head.module.css";
+import { useCustomSelector } from "../../../store/hooks";
 
 export default function Head() {
+  const username = useCustomSelector((state) => state.auth.user);
   return (
     <div className={style.principalContainer}>
       <div className={style.logoContainer}>
         <LogoPomo className={style.logo} />
-        <h1>Pomo Pos</h1>
+        <h1 className={style.identifyContainer__h1}>Pomo Pos</h1>
+      </div>
+      <div className={style.companyContainer}>
+        <MarketIcon className={style.icon} />
+        <h2 className={style.identifyContainer__h2}>La esquina del sabor</h2>
       </div>
       <div className={style.identifyContainer}>
-        <h3 className={style.identifyContainer__h3}>User</h3>
-        <MarketIcon className={style.icon} />
-        <h2>La esquina del sabor</h2>
+        <h3 className={style.identifyContainer__h3}>{username}</h3>
       </div>
     </div>
   );
