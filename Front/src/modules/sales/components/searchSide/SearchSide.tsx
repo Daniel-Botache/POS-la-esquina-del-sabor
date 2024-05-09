ae6deb105249a2ff76b5a94776ce3a68e9ca65c9
Daniel-Botache <daniel.botache@gmail.com>
2024-05-09 12:29:48 -0500

    se agrega estilos para el head del searchside

--- Front/src/modules/sales/components/searchSide/SearchSide.tsx
+++ Front/src/modules/sales/components/searchSide/SearchSide.tsx
@@ -1,13 +1,17 @@
 import Cards from "./Cards";
 import SeachBar from "../../../searchBar/components/SearchBar";
 import style from "../../styles/SearchSide.module.css";
+import { AddIcon } from "../../../../utils/Icons/icons";
 
 export default function SearchSide() {
   return (
     <div className={style.principalContainer}>
       <div className={style.headContainer}>
         <SeachBar></SeachBar>
-        <button>Agregar Producto</button>
+        <div className={style.headContainer__button}>
+          <p className={style.headContainer__button__p}>Crear Producto</p>
+          <AddIcon className={style.headContainer__button__icon} />
+        </div>
       </div>
       <Cards />
     </div>
