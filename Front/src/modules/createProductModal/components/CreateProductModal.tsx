1bfd83ee272e3f8076acf344346113f78dab8f58
Daniel-Botache <daniel.botache@gmail.com>
2024-05-19 16:16:16 -0500

    se mejoran los estiulos para la creacion de productos

--- Front/src/modules/createProductModal/components/CreateProductModal.tsx
+++ Front/src/modules/createProductModal/components/CreateProductModal.tsx
@@ -2,6 +2,7 @@ import style from "../styles/CreateProductModal.module.css";
 import { useState, useEffect } from "react";
 import { useCustomSelector, useCustomDispatch } from "../../../store/hooks";
 import { getSuppliers } from "../redux/createProductSlice";
+import { SearchIcon } from "../../../utils/Icons/icons";
 
 export default function CreateProductModal() {
   const [selectedSuppliers, setSelectedSuppliers] = useState<string[]>([]);
@@ -24,18 +25,19 @@ export default function CreateProductModal() {
     <div className={style.modalOverlay}>
       <div className={style.principalContainer}>
         <form action="submit" className={style.formContainer}>
-          <h2>Crear Producto</h2>
-          <div>
+          <h2>Crear o editar producto</h2>
+          <div className={style.inputContainer}>
             <label htmlFor="inputName" className={style.form__label}>
-              Nombre
+              Id
             </label>
             <input
+              disabled
               type="text"
               id="inputName"
               className={style.form__inputText}
             />
           </div>
-          <div>
+          <div className={style.inputContainer}>
             {" "}
             <label htmlFor="inputBarCode" className={style.form__label}>
               Cod. Barras
@@ -46,7 +48,18 @@ export default function CreateProductModal() {
               className={style.form__inputText}
             />
           </div>
-          <div>
+          <div className={style.inputContainer}>
+            <label htmlFor="inputName" className={style.form__label}>
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="inputName"
+              className={style.form__inputText}
+            />
+          </div>
+
+          <div className={style.inputContainer}>
             <label htmlFor="inputPrice" className={style.form__label}>
               Precio
             </label>
@@ -56,36 +69,79 @@ export default function CreateProductModal() {
               className={style.form__inputText}
             />
           </div>
+          <div className={style.inputContainer}>
+            {" "}
+            <label htmlFor="inputVolume" className={style.form__label}>
+              Inventario
+            </label>
+            <input
+              type="text"
+              id="inputVolume"
+              className={style.form__inputText}
+            />
+          </div>
+          <div className={style.inputContainer}>
+            <label htmlFor="inputMaximum" className={style.form__label}>
+              Tope
+            </label>
+            <input
+              type="text"
+              name=""
+              id="inputMaximum"
+              className={style.form__inputText}
+            />
+          </div>
+          <div className={style.inputContainer}>
+            <label htmlFor="inputType" className={style.form__label}>
+              Clasificación
+            </label>
+            <div>
+              {" "}
+              <input
+                type="text"
+                id="inputType"
+                className={style.form__inputText}
+              />
+              <button className={style.principalContainer__btn}>
+                <SearchIcon className={style.principalContainer__icon} />
+              </button>
+            </div>
+          </div>
+          <div className={style.inputContainer}>
+            {" "}
+            <label htmlFor="inputImg" className={style.form__label}>
+              URL Imagen
+            </label>
+            <input
+              type="text"
+              id="inputImg"
+              className={style.form__inputText}
+            />
+          </div>
+          <div className={style.inputContainer}>
+            <div>
+              <label htmlFor="inputSpent" className={style.form__label}>
+                Verdura
+              </label>
+              <input type="checkbox" name="" id="inputSpent" />
+            </div>
+            <div>
+              <label htmlFor="individualRadio">
+                Individual
+                <input
+                  type="radio"
+                  name="tipo"
+                  id="individualRadio"
+                  value="individual"
+                />
+              </label>
+              <label htmlFor="pacaRadio">
+                Paca
+                <input type="radio" name="tipo" id="pacaRadio" value="paca" />
+              </label>
+            </div>
+          </div>
 
-          <label htmlFor="inputVolume" className={style.form__label}>
-            Inventario
-          </label>
-          <input
-            type="text"
-            id="inputVolume"
-            className={style.form__inputText}
-          />
-          <label htmlFor="inputMaximum" className={style.form__label}>
-            Tope
-          </label>
-          <input
-            type="text"
-            name=""
-            id="inputMaximum"
-            className={style.form__inputText}
-          />
-          <label htmlFor="inputType" className={style.form__label}>
-            Clasificación
-          </label>
-          <input type="text" id="inputType" className={style.form__inputText} />
-          <label htmlFor="inputImg" className={style.form__label}>
-            URL Imagen
-          </label>
-          <input type="text" id="inputImg" className={style.form__inputText} />
-          <label htmlFor="inputSpent" className={style.form__label}>
-            Verdura
-          </label>
-          <input type="checkbox" name="" id="inputSpent" />
           <label htmlFor="inputSuppliers" className={style.form__label}>
             Proveedores
           </label>
@@ -94,6 +150,7 @@ export default function CreateProductModal() {
             id="inputSuppliers"
             value={selectedSuppliers}
             onChange={handleSupplierSelection}
+            className={style.form__select}
           >
             {suppliers.map((supplier) => (
               <option key={supplier.id} value={supplier.id}>
@@ -101,7 +158,9 @@ export default function CreateProductModal() {
               </option>
             ))}
           </select>
-          <button type="submit">Crear producto</button>
+          <button type="submit" className={style.formContainer__btn}>
+            Crear producto
+          </button>
         </form>
       </div>
     </div>
