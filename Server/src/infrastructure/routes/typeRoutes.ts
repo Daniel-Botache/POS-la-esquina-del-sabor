db7df06671139a2ef1ed553dee3ed12293d1f2dd
Daniel-Botache <daniel.botache@gmail.com>
2024-05-23 11:55:55 -0500

    se generan las rutas para el typeController

---
+++ Server/src/infrastructure/routes/typeRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { TypeController } from "../webserver/TypeController";
+
+const router = Router();
+const controller = new TypeController();
+
+router.get("/", controller.getAllData);
+router.get("/:id", controller.findSuplierById);
+router.post("/", controller.createData);
+router.put("/:id", controller.putData);
+router.delete("/:id", controller.deleteData);
+
+export const suplierRoutes = router;
