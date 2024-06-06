0c48d7b5b27ffa8553df5ac7c27541bbb7d19eb2
Daniel-Botache <daniel.botache@gmail.com>
2024-06-06 17:09:54 -0500

    se aplican los estilos a la barra lateral para que se indique donde se encuentra el usuario

--- Front/src/modules/navBarLeft/components/navBarLeft.tsx
+++ Front/src/modules/navBarLeft/components/navBarLeft.tsx
@@ -21,43 +21,91 @@ export default function NavBarLeft() {
     <div className={style.principalContainer}>
       <Link to="/sales">
         <div title="Ventas">
-          <VentasIcon className={style.principalContainer__icon} />
+          <VentasIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/sales"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
       <Link to="/stock">
         <div title="Inventario">
-          <InvetarioIcon className={style.principalContainer__icon} />
+          <InvetarioIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/stock"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
       <Link to="/credits">
         <div title="Créditos">
-          <CreditoIcon className={style.principalContainer__icon} />
+          <CreditoIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/credits"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
       <Link to="/profit">
         <div title="Ganancias">
-          <GananciasIcon className={style.principalContainer__icon} />
+          <GananciasIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/profit"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
       <Link to="/expenses">
         <div title="Gastos">
-          <GastosIcon className={style.principalContainer__icon} />
+          <GastosIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/expenses"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
       <Link to="/balance">
         <div title="Balance">
-          <BalanceIcon className={style.principalContainer__icon} />
+          <BalanceIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/balance"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
       <Link to="/bases">
         {" "}
         <div title="Bases">
-          <BaseIcon className={style.principalContainer__icon} />
+          <BaseIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/bases"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
       <Link to="/options">
         <div title="Configuración">
-          <ConfiguracionIcon className={style.principalContainer__icon} />
+          <ConfiguracionIcon
+            className={`${style.principalContainer__icon} ${
+              location.pathname == "/options"
+                ? style.principalContainer__icon_scale
+                : ""
+            }`}
+          />
         </div>
       </Link>
     </div>
