dbef190a9a1581d4839f877ffcd47e6f88300193
Daniel-Botache <daniel.botache@gmail.com>
2024-06-07 12:17:54 -0500

    se agrega la tabla y la cell para el profit

---
+++ Front/src/modules/profit/components/CellProfit.tsx
@@ -0,0 +1,9 @@
+import style from "../styles/CellProfit.module.css";
+
+export default function CellProfit() {
+  return (
+    <div className={style.principalContainer}>
+      <div className={style.prepertyContainer}></div>
+    </div>
+  );
+}
---
+++ Front/src/modules/profit/components/TablePorfit.tsx
@@ -0,0 +1,11 @@
+import style from "../styles/TableProfit.module.css";
+import CellProfit from "./CellProfit";
+import { useCustomSelector } from "../../../store/hooks";
+
+export default function TableProfit() {
+  return (
+    <div className={style.principalContainer}>
+      <div></div>
+    </div>
+  );
+}
