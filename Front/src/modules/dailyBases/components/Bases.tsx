09f4b7494e3661c4e04344f30ccbbdbaff25c0e9
Daniel-Botache <daniel.botache@gmail.com>
2024-07-06 00:15:13 -0500

    se crea un nuevo input para seleccionar el rango de fechas

--- Front/src/modules/dailyBases/components/Bases.tsx
+++ Front/src/modules/dailyBases/components/Bases.tsx
@@ -5,24 +5,42 @@ import CreateBaseModal from "./CreateBaseModal";
 
 export default function Bases() {
   const [filterInitialDate, setFilterInitialDate] = useState("");
+  const [filterFinalDate, setFilterFinalDate] = useState("");
+  const [typeSelected, setTypeSelected] = useState("");
   const [createBaseModalOpen, setCreateBaseModalOpen] = useState(false);
 
   const toggleModalBase = () => {
     setCreateBaseModalOpen(!createBaseModalOpen);
   };
+
+  const typeSelectedHandle = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const typeInput = event.target.value;
+    setTypeSelected(typeInput);
+  };
+
   return (
     <div className={style.principalContainer}>
       <div className={style.headContainer}>
         <div className={style.optionContainer}>
           <div className={style.inputContainer}>
             <label htmlFor="desdeDate" className={style.inputContainer__label}>
-              Fecha:
+              Desde:
             </label>
             <input
               onChange={(e) => setFilterInitialDate(e.target.value)}
               type="date"
               name=""
               id="desdeDate"
+              className={style.inputContainer__input}
+            />
+            <label htmlFor="hastaDate" className={style.inputContainer__label}>
+              Hasta:
+            </label>
+            <input
+              onChange={(e) => setFilterFinalDate(e.target.value)}
+              type="date"
+              name=""
+              id="hastaDate"
               className={style.inputContainer__input}
             />
           </div>
@@ -39,6 +57,7 @@ export default function Bases() {
             name="suplierInput"
             id="suplierInput"
             className={style.optionContainer__select}
+            onChange={typeSelectedHandle}
           >
             <option value="">Todos</option>
             <option value="Base">Base</option>
--- Server/src/infrastructure/routes/baseRoutes.ts
+++ Server/src/infrastructure/routes/baseRoutes.ts
@@ -6,8 +6,8 @@ const router = Router();
 const controller = new BaseController();
 
 router.get("/", controller.getAllData);
-router.get("/:id", controller.getDataById);
 router.get("/baseToday", controller.findBaseDateNow);
+router.get("/:id", controller.getDataById);
 router.post("/baseDate", controller.findSBaseByDate);
 router.post("/", controller.createData);
 router.put("/:id", controller.putData);
