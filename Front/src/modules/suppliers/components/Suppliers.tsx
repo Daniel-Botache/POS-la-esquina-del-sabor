bec30185124f90cefbe823ebc88a529b23ad6942
Daniel-Botache <daniel.botache@gmail.com>
2024-08-07 06:16:13 -0500

    se agrega la funcion para ordenar los proveedores por nombre

--- Front/src/modules/suppliers/components/Suppliers.tsx
+++ Front/src/modules/suppliers/components/Suppliers.tsx
@@ -3,13 +3,39 @@ import SearchBarSupplier from "./SearchBarSupplier";
 import { AddIcon } from "../../../utils/Icons/icons";
 import CreateSupplierModal from "../../createSupplierModal/components/CreateSupplierModal";
 import { useState } from "react";
+import { useCustomSelector, useCustomDispatch } from "../../../store/hooks";
+import { Supplier } from "../redux/supplierSlice";
+import { addSupplierCopy } from "../redux/supplierSlice";
 
 export default function Suppliers() {
   const [isModalCreateSupplierOpen, setIsModalCreateSupplierOpen] =
     useState(false);
+  const [typeSort, setTypeSort] = useState("");
+  const dispatch = useCustomDispatch();
 
   const toggleModalCreateSupplier = () => {
     setIsModalCreateSupplierOpen(!isModalCreateSupplierOpen);
+  };
+
+  const suppliersCopy = useCustomSelector(
+    (state) => state.suppliers.suppliersCopy
+  );
+
+  const sortByName = (event: React.MouseEvent<HTMLHRElement>) => {
+    event.preventDefault();
+    if (typeSort == "nameSort") {
+      setTypeSort("");
+      const arraySorted = [...suppliersCopy].sort((a: Supplier, b: Supplier) =>
+        b.company?.localeCompare(a.company)
+      );
+      dispatch(addSupplierCopy({ suppliersCopy: arraySorted }));
+      return;
+    }
+    setTypeSort("nameSort");
+    const arraySorted = [...suppliersCopy].sort((a: Supplier, b: Supplier) =>
+      a.company?.localeCompare(b.company)
+    );
+    dispatch(addSupplierCopy({ suppliersCopy: arraySorted }));
   };
   return (
     <div className={style.principalContainer}>
@@ -24,6 +50,30 @@ export default function Suppliers() {
             <p className={style.headContainer__button__p}>Crear Proveedor</p>
           </div>
         </div>
+      </div>
+      <div className={style.titleContainer}>
+        <h3 className={style.titleContainer__h3} onClick={sortByName}>
+          Empresa:{" "}
+          <span className={style.titleCOntainer__span}>
+            {typeSort == "nameSort" ? "▼" : "▶"}
+          </span>
+        </h3>
+        <h3 className={style.titleContainer__h3}>Asesor: </h3>
+        <h3 className={style.titleContainer__h3}>Teléfono:</h3>
+        <h3 className={style.titleContainer__h3}>
+          Fecha de Creación:{" "}
+          <span className={style.titleCOntainer__span}>
+            {typeSort == "lastPaymentSort" ? "▼" : "▶"}
+          </span>
+        </h3>
+        <h3 className={style.titleContainer__h3}>
+          Total de productos:{" "}
+          <span className={style.titleCOntainer__span}>
+            {typeSort == "remainingQuotaSort" ? "▼" : "▶"}
+          </span>
+        </h3>
+
+        <h3 className={style.titleContainer__h3}>Acciones:</h3>
       </div>
       {isModalCreateSupplierOpen && (
         <CreateSupplierModal onClose={toggleModalCreateSupplier} />
